537d0c3c580a6245cbb06a18d989e076ff18faec
Angel Gutierrez <angel50710@hotmail.com>
2020-07-27 08:39:47 -0500

    configurando variable de entorno de PORT

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
+require('./config/config.js')
 const express = require("express");
 const hbs = require("hbs");
 
 const app = express();
 
 app.use(express.static(__dirname + '/public'))
-app.set('view engine','hbs');
+app.set('view engine', 'hbs');
 
 hbs.registerPartials(__dirname + '/views/partials/');
 
+//Rutas
 app.use(require('./routes/index.js'))
 
-app.listen(3000, (err) => {
-  if (err) {
-    return console.log(err);
-  }
-  console.log("Servidor ejecutandose en el puerto 3000");
+//configuracion de puertos
+app.listen(process.env.PORT, (err) => {
+    if (err) {
+        return console.log(err);
+    }
+    console.log(`Servidor ejecutandose en el puerto ${process.env.PORT}`);
 });
