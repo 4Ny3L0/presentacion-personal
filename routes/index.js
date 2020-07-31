d9463edc0a1f236cbdc6a5ecb322b3686d6b90b7
Angel Gutierrez <angel50710@hotmail.com>
2020-07-31 17:57:49 -0500

    comente el llamado a la clase enviarMail, para trabajar con el formulario

--- routes/index.js
+++ routes/index.js
@@ -29,8 +29,8 @@ app.get("/contacto", (req, res) => {
 app.post('/contacto', (req, res) => {
     let body = req.body;
     // receptor.text = body;
-    // console.log(body, receptor)
-    enviarMail(body)
+    console.log(body)
+        // enviarMail(body)
     res.render('contacto')
 })
 module.exports = app;
