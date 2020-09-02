d12fd8b0b2ea5f9a470aaffec4eb97de5481bc55
Angel Gutierrez <angel50710@hotmail.com>
2020-09-02 16:04:57 -0500

    cambiando los iconos de redes sociales

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 process.env.PORT = process.env.PORT || 3000;
-process.env.EUSER = process.env.EUSER || 'sendmeamail507@gmail.com';
-process.env.PASS = process.env.PSS || 'AgG00gL3.06.G';
+process.env.EUSER = process.env.EUSER || "sendmeamail507@gmail.com";
+process.env.PASS = process.env.PSS;
 
 euser = process.env.EUSER;
 ps = process.env.PASS;
 
-module.exports = euser, ps;
+(module.exports = euser), ps;
